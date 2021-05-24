37aca2c5e8d37e006cd5afcb04bb5a991718d8f7
李鑫 <516868419@qq.com>
2021-05-24 15:55:24 +0800

    修改ScreenUtils计算高度

--- tuikit/src/main/java/com/tencent/qcloud/tim/uikit/utils/ScreenUtil.java
+++ tuikit/src/main/java/com/tencent/qcloud/tim/uikit/utils/ScreenUtil.java
@@ -1,10 +1,12 @@
 package com.tencent.qcloud.tim.uikit.utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.Rect;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -12,7 +14,10 @@ import android.os.Build;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -74,15 +79,68 @@ public class ScreenUtil {
         return result;
     }
 
-    public static int getScreenHeight(Context context) {
+    public static int getScreenHeight(@Nullable Context context) {
+        if (context == null)
+            return 0;
+        //高度不准
+        //        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+        //        return dm.heightPixels;
+
         DisplayMetrics metric = new DisplayMetrics();
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-//        wm.getDefaultDisplay().getMetrics(metric); //高度不准
         wm.getDefaultDisplay().getRealMetrics(metric);
+
+        //判断是否开启了虚拟按键
+        if (checkDeviceHasNavigationBar(context)) {
+            int navBarHeight = getNavigationBarHeight(context);
+            return metric.heightPixels - navBarHeight;
+        }
+
         return metric.heightPixels;
+
     }
 
+    /**
+     * 获取是否有虚拟按键
+     * 通过判断是否有物理返回键反向判断是否有虚拟按键
+     *
+     * @param context
+     * @return
+     */
+    public static boolean checkDeviceHasNavigationBar(Context context) {
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
+            Point size = new Point();
+            Point realSize = new Point();
+            display.getSize(size);
+            display.getRealSize(realSize);
+            boolean result = realSize.y != size.y;
+            return realSize.y != size.y;
+        } else {
+
+            boolean menu = ViewConfiguration.get(((Activity) context)).hasPermanentMenuKey();
+            boolean back = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_BACK);
+            if (menu || back) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+    //获取虚拟按键的高度
+    public static int getNavigationBarHeight(Context context) {
+        int result = 0;
+        if (checkDeviceHasNavigationBar(context)) {
+            Resources res = context.getResources();
+            int resourceId = res.getIdentifier("navigation_bar_height", "dimen", "android");
+            if (resourceId > 0) {
+                result = res.getDimensionPixelSize(resourceId);
+            }
+        }
+        return result;
+    }
 
 
     public static int getScreenWidth(Context context) {
