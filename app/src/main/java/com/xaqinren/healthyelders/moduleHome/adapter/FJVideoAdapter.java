13a4af8319eb8c0439669dfa026be15ba28e159a
李鑫 <516868419@qq.com>
2021-06-09 20:25:01 +0800

    修改首页滑动变化附近裂开问题

--- app/src/main/java/com/xaqinren/healthyelders/moduleHome/adapter/FJVideoAdapter.java
+++ app/src/main/java/com/xaqinren/healthyelders/moduleHome/adapter/FJVideoAdapter.java
@@ -42,7 +42,6 @@ public class FJVideoAdapter extends BaseQuickAdapter<VideoInfo, BaseViewHolder> 
             int newHeight = itemWidth * oldHeight / oldWidth;
 
             RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) binding.rlItem.getLayoutParams();
-            params.width = itemWidth;
             params.height = newHeight;
             binding.rlItem.setLayoutParams(params);
 
